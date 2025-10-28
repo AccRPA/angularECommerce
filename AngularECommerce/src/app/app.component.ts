b80cad4118afe838affe6f6cf26bb9abe7160b6a
AccRPA <accrpaw10@gmail.com>
2025-10-28 21:37:30 +0100

    feat: added bootstrap and bootstrap icons

--- AngularECommerce/src/app/app.component.ts
+++ AngularECommerce/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { NgbDateStruct, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [NgbDatepickerModule, FormsModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'AngularECommerce';
+  model: NgbDateStruct | undefined;
+  date: { year: number; month: number; } | undefined;
 }
