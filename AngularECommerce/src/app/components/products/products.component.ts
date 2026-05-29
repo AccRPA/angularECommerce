b5533c1a2433998326a785fe8bbb55ff484c980e
AccRPA <accrpaw10@gmail.com>
2026-05-29 18:16:55 +0200

    fix: cache for pagination and product list

--- AngularECommerce/src/app/components/products/products.component.ts
+++ AngularECommerce/src/app/components/products/products.component.ts
@@ -3,6 +3,7 @@ import { ProductsService } from '../../services/products.service';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
 import { CartService } from '../../services/cart.service';
 import { RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -18,20 +19,28 @@ import { PaginatorModule, PaginatorState } from 'primeng/paginator';
     CardModule,
     RouterModule,
     AsyncPipe,
-    PaginatorModule
+    PaginatorModule,
+    SkeletonModule
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.sass'
 })
 export class ProductsComponent {
   products$: Observable<ProductModel[] | undefined> | undefined;
-  first: number = 0;
-  rows: number = 10;
-  page: number = 0;
+  first: number;
+  rows: number;
+  page: number;
+  total:number;
   rowsPerPage = [10, 20, 30];
+  showLoader: boolean = false;
 
   constructor(private productSrv: ProductsService,
-    private cartSrv: CartService){}
+    private cartSrv: CartService){
+      this.first = this.productSrv.getPagination()?.first;
+      this.rows = this.productSrv.getPagination()?.rows;
+      this.page = this.productSrv.getPagination()?.page;
+      this.total = this.productSrv.getPagination()?.pageCount;
+    }
 
   ngOnInit(){
     this.setProducts();
@@ -45,10 +54,16 @@ export class ProductsComponent {
     this.first = event.first || 0;
     this.rows = event.rows || 0;
     this.page = event.page || 0;
+    this.productSrv.setPagination({
+      first: this.first,
+      rows: this.rows,
+      page: this.page,
+      pageCount: this.productSrv.getPagination()?.pageCount
+    });
     this.setProducts();
   }
   
   private setProducts(){
-    this.products$ = this.productSrv.getProducts(this.rows, this.rows * this.page);
+    this.products$ = this.productSrv.getCacheProducts(this.rows, this.rows * this.page);
   }
 }
--- AngularECommerce/src/app/services/products.service.ts
+++ AngularECommerce/src/app/services/products.service.ts
@@ -1,26 +1,66 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 import { ProductsModel } from '../models/products.model';
 import { ProductDetailsModel } from '../models/product-details.model';
-import { response } from 'express';
 import { ProductModel } from '../models/product.model';
+import { PageEventModel } from '../models/pageEvent.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
 
+  private cacheProducts: ProductModel[] = [];
+  private pagination: PageEventModel = {
+    first: 0,
+    rows: 10,
+    page: 0,
+    pageCount: 194
+  };
+
   constructor(private httpClient: HttpClient) { }
 
   getProducts(limit: number, skip: number): Observable<ProductModel[] | undefined>{
     return this.httpClient.get<ProductsModel>('https://dummyjson.com/products?limit=' + limit + '&skip=' + skip)
     .pipe(
-      map((response: ProductsModel) => response.products)
+      map((response: ProductsModel) => {
+        this.setCacheProducts(response.products);
+        return response.products;
+      })
     );
   }
 
   getProductDetails(id: string): Observable<ProductDetailsModel>{
     return this.httpClient.get<ProductDetailsModel>('https://dummyjson.com/products/' + id);
+  }
+
+  setCacheProducts(products: ProductModel[] | undefined){
+    if (products?.length){
+      this.cacheProducts.push(...products);
+    }
+  }
+
+  getCacheProducts(limit: number, skip: number): Observable<ProductModel[] | undefined>{
+    const result = this.cacheProducts.slice(skip, skip + limit);
+    if (!result?.length){
+      return this.getProducts(limit, skip);
+    }
+    return of(result);
+  }
+
+  clearCacheProducts(){
+    this.cacheProducts = [];
+  }
+
+  setPagination(page: PageEventModel){
+    if (this.pagination.rows !== page.rows){
+      this.clearCacheProducts();
+    }
+    this.pagination = page;
+  }
+
+  getPagination(){
+    return this.pagination;
   }
 }
