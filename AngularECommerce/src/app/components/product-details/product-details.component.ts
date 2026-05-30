51a346c970caa603630bc1823adad2f3a9b964cf
AccRPA <accrpaw10@gmail.com>
2026-05-30 15:51:04 +0200

    feat: layout for product details

--- AngularECommerce/src/app/components/product-details/product-details.component.ts
+++ AngularECommerce/src/app/components/product-details/product-details.component.ts
@@ -2,16 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductsService } from '../../services/products.service';
 import { Observable, Subscription } from 'rxjs';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe, JsonPipe } from '@angular/common';
 import { ProductDetailsModel } from '../../models/product-details.model';
 import { GalleriaModule } from 'primeng/galleria';
+import { SkeletonModule } from 'primeng/skeleton';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-product-details',
   imports: [
     JsonPipe,
     AsyncPipe,
     RouterLink,
-    GalleriaModule
+    FormsModule,
+    GalleriaModule,
+    SkeletonModule,
+    CurrencyPipe,
+    RatingModule
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.sass'
